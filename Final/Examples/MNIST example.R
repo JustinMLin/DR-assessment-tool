6ab47299f7eabdc22b5b86314e9e6cfd64728d39
Justin Lin <linjus@iu.edu>
2024-06-25 10:01:45 -0400

    Update manuscript

--- Final/Examples/MNIST example.R
+++ Final/Examples/MNIST example.R
@@ -32,7 +32,7 @@ run_app(Z_dist, X, real_labels, id)
 p = data.frame(x=X[,1], y=X[,2], cluster=real_labels, id) %>%
   ggplot(aes(x=x, y=y, color=factor(cluster))) +
   geom_point(size=0.5) +
-  labs(color="Class")
+  labs(title="UMAP embedding of MNIST data set", x="", y="", color="Digit")
 
 p
 
@@ -41,3 +41,11 @@ p
 kmeans_cluster = kmeans(Z_pca, centers=10, iter.max=100, nstart=20)$cluster
 
 run_app(Z_dist, X, kmeans_cluster, id)
+
+q = data.frame(x=X[,1], y=X[,2], cluster=factor(kmeans_cluster, levels=c(7, 2, 6, 10, 3, 1, 9, 5, 4, 8)), id) %>%
+  ggplot(aes(x=x, y=y, color=factor(cluster))) +
+  geom_point(size=0.5) +
+  scale_color_discrete(labels=as.character(1:10)) +
+  labs(title="UMAP embedding of MNIST data set", x="", y="", color="Class")
+
+q
--- MST robustness/print mst.R
+++ MST robustness/print mst.R
@@ -4,25 +4,36 @@ library(igraph)
 
 source("~/Desktop/Research/DR-assessment-tool/Final/DR tool functions final.R")
 
-X = rbind(mvrnorm(n=100, mu=c(0,0), Sigma=diag(c(1,1))),
-          mvrnorm(n=100, mu=c(5,0), Sigma=diag(c(1,1))),
-          mvrnorm(n=100, mu=c(2.5,2.5), Sigma=diag(c(1,1))))
+set.seed(1323)
+
+X = rbind(mvrnorm(n=100, mu=c(0,0), Sigma=diag(c(2,2))),
+          mvrnorm(n=100, mu=c(4,0), Sigma=diag(c(2,2))))
+Y = rbind(X, mvrnorm(n=100, mu=c(2,5), Sigma=diag(c(2,2))))
+
 label = c(rep(1, 100), rep(2, 100), rep(3,100))
 
-df = data.frame(x=X[,1], y=X[,2], label)
-
-graph = ggplot(df, aes(x=x, y=y, color=factor(label))) +
-  geom_point()
-
-mst = get_mst(dist(X))
-
-for (i in 1:length(E(mst))) {
-  tail = tail_of(mst, E(mst)[i])
-  head = head_of(mst, E(mst)[i])
+print_mst = function(X, cluster) {
+  df = data.frame(x=X[,1], y=X[,2], label=cluster)
   
-  df_temp = df[c(tail, head),]
+  graph = ggplot(df, aes(x=x, y=y, color=factor(label))) +
+    geom_point() +
+    scale_color_manual(values=hue_pal()(3)[1:length(unique(cluster))])
   
-  graph = graph + geom_segment(data=df_temp, aes(xend=lead(x), yend=lead(y)), color="black")
+  mst = get_mst(dist(X))
+  
+  for (i in 1:length(E(mst))) {
+    tail = tail_of(mst, E(mst)[i])
+    head = head_of(mst, E(mst)[i])
+    
+    df_temp = df[c(tail, head),]
+    
+    graph = graph + geom_segment(data=df_temp, aes(xend=lead(x), yend=lead(y)), color="black")
+  }
+  
+  graph
 }
 
-graph
+print_mst(X, label[1:200])
+print_mst(Y, label)
+
+
